940cb9fcb8e0b3828dc59b6f6447a9bfcb00e485
koyel120601 <117771331+koyel120601@users.noreply.github.com>
2023-04-10 20:21:24 +0530

    my second commit

--- Backend/flasks.py
+++ Backend/flasks.py
@@ -11,7 +11,7 @@ import pickle
 
 #creating an instance of flask
 app = Flask(__name__)
-CORS(app,origins=["http://localhost:3000"])
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route("/",methods=['POST'])
